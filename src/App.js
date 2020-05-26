6f64d614bbb1e8b7138464434ecdb947532ede0f
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-05-26 08:42:50 +0530

    baic componants

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Welcome from './componants/Welcome';
+import Greeting from './componants/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
